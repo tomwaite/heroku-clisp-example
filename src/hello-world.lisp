fe6c0012e55349cc339a527cdbc65b86414d6863
Tom Waite <tomwaite@acm.org>
2019-01-25 19:08:59 +0000

    fix parsing of url, localise getenv call

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -8,6 +8,7 @@
 ;; Database
 (defvar *database-url* (heroku-getenv "DATABASE_URL"))
 
+
 (defun db-params ()
   "Heroku database url format is postgres://username:password@host/database_name.
 TODO: cleanup code."
@@ -18,9 +19,23 @@ TODO: cleanup code."
 	 (database (second (cl-ppcre:split "/" (second (cl-ppcre:split "@" url))))))
     (list database user password host)))
 
+
+(defun db-dyn-params (the-url)
+  "Heroku database url format is postgres://username:password@host:port/database_name.
+TODO: cleanup code."
+  (let* ((url (second (cl-ppcre:split "//" the-url)))
+	 (user (first (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
+	 (password (second (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
+	 (host (first (cl-ppcre:split ":" (second (cl-ppcre:split "@" url)))))
+	 (database (second (cl-ppcre:split "/" (second (cl-ppcre:split "@" url))))))
+    (list database user password host)))
+
+
+
 ;; Handlers
 (push (hunchentoot:create-folder-dispatcher-and-handler "/static/" "/app/public/")
 	 hunchentoot:*dispatch-table*)
+
 
 (hunchentoot:define-easy-handler (hello-sbcl :uri "/") ()
   (cl-who:with-html-output-to-string (s)
@@ -40,6 +55,7 @@ TODO: cleanup code."
        (:a :href "static/hello.txt" "hello"))
       (:h3 "App Database")
       (:div
-       (:pre "SELECT version();"))
-      (:div (format s "~A" (postmodern:with-connection (db-params)
-			     (postmodern:query "select version()"))))))))
+       (:pre "SELECT version();")))      
+     (:div (format s "~A" 
+                    (postmodern:with-connection (db-dyn-params (heroku-getenv "DATABASE_URL"))
+                      (postmodern:query "select version()")))))))
